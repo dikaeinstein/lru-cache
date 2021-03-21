9eefc2769ac951237d51b8aa82176495e71c71d7
Dika Okwa <solozyokwa@gmail.com>
2021-03-21 03:31:30 +0100

    Update store.go

--- store.go
+++ store.go
@@ -14,19 +14,19 @@ type Store interface {
 }
 
 // InMemoryStore is an in-memory implementation of Store.
-// Which is save for concurrent use
+// Which is safe for concurrent use.
 type InMemoryStore struct {
 	sync.RWMutex
 	store map[string]*list.Element
 }
 
-// NewInMemoryStore creates a new in-memory store
+// NewInMemoryStore creates a new in-memory store.
 func NewInMemoryStore() Store {
 	store := make(map[string]*list.Element)
 	return &InMemoryStore{store: store}
 }
 
-// Get returns the value for the given key from the store
+// Get returns the value for the given key from the store.
 func (i *InMemoryStore) Get(key string) (*list.Element, bool) {
 	i.RLock()
 	defer i.RUnlock()
@@ -35,14 +35,14 @@ func (i *InMemoryStore) Get(key string) (*list.Element, bool) {
 	return e, exist
 }
 
-// Set saves the given value for the key in the store
+// Set saves the given value for the key in the store.
 func (i *InMemoryStore) Set(key string, value *list.Element) {
 	i.Lock()
 	i.store[key] = value
 	i.Unlock()
 }
 
-// Delete removes the given value for the key from the store
+// Delete removes the given value for the key from the store.
 func (i *InMemoryStore) Delete(key string) {
 	i.Lock()
 	delete(i.store, key)
